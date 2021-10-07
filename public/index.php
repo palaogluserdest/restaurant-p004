03f8bca7cdef9e03587f5f2a57e7eafaa2cbad89
serdest palaoglu <palaogluserdest@gmail.com>
2021-10-07 23:08:00 +0300

    slider-1 eklendi

--- public/index.php
+++ public/index.php
@@ -3,17 +3,19 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <title>Hello, world!</title>
 </head>
 <body>
 
-<div class="container mt-2">
 
-    <div id="navbar">
+<div id="navbar" class="mt-2">
+    <div class="container">
         <div class="row">
             <div class="col-4">
                 <img id="logo" src="img/LOGO.png" alt="">
@@ -29,20 +31,89 @@
             </div>
         </div>
     </div>
+</div>
 
-    <hr>
+<hr>
 
-    <div id="main">
-        <div style="width: 100%; height: 250px">slider</div>
-        <div style="width: 100%; height: 500px">best dishes</div>
+<div id="main">
+    <div class="container">
+       <div id="slider-1" class="carousel slide" data-ride="carousel">
+
+           <ol class="carousel-indicators">
+               <li data-target="#slider-1" data-slide-to="0" class="active"></li>
+               <li data-target="#slider-1" data-slide-to="1"></li>
+               <li data-target="#slider-1" data-slide-to="2"></li>
+           </ol>
+
+           <div class="carousel-inner">
+               <div class="carousel-item active">
+                   <div class="row">
+                       <div class="col-1"></div>
+                       <div class="col-7" style="margin-left: 35px;">
+                           <h4>What is Pasta?</h4>
+                           <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                               dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                               perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                               nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                               Ducimus, repellendus?</p>
+                       </div>
+                       <div class="col-3">
+                           <img src="img/slideimg.png" alt="" class="d-block w-100">
+                       </div>
+                       <div class="col-1"></div>
+                   </div>
+               </div>
+               <div class="carousel-item">
+                   <div class="row">
+                       <div class="col-1"></div>
+                       <div class="col-7" style="margin-left: 35px;">
+                           <h4>What is Pasta?</h4>
+                           <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                               dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                               perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                               nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                               Ducimus, repellendus?</p>
+                       </div>
+                       <div class="col-3">
+                           <img src="img/slideimg.png" alt="" class="d-block w-100">
+                       </div>
+                       <div class="col-1"></div>
+                   </div>
+               </div>
+               <div class="carousel-item">
+                   <div class="row">
+                       <div class="col-1"></div>
+                       <div class="col-7" style="margin-left: 35px;">
+                           <h4>What is Pasta?</h4>
+                           <p class="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque autem blanditiis corporis culpa debitis
+                               dicta distinctio dolor dolore earum, eius eveniet explicabo fugit harum illo iste iure iusto modi nostrum odio optio pariatur
+                               perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem. Ducimus, repellendus?
+                               nostrum odio optio pariatur perferendis, perspiciatis placeat quas quia quibusdam quod quos saepe sed sit, vel veritatis voluptatem.
+                               Ducimus, repellendus?</p>
+                       </div>
+                       <div class="col-3">
+                           <img src="img/slideimg.png" alt="" class="d-block w-100">
+                       </div>
+                       <div class="col-1"></div>
+                   </div>
+               </div>
+
+
+                  <a href="#slider-1" class="carousel-control-prev" data-slide="prev">
+                      <span class="arrow"><i class="fas fa-chevron-left fa-2x"></i></span>
+                  </a>
+                   <a href="#slider-1" class="carousel-control-next" data-slide="next">
+                       <span class="arrow"><i class="fas fa-chevron-right fa-2x"></i></span>
+                   </a>
+
+           </div>
+       </div>
     </div>
-
-
 </div>
 
 
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
